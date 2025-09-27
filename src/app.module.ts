a2a887700d46379651fc5463d717d7f54c40536b
rogerinfas <rinfas@ulasalle.edu.pe>
2025-09-27 01:04:13 -0500

    refactor(config): move hardcoded passwords to environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Para env
+    }),
+    TypeOrmModule.forRoot({ //Para db
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
